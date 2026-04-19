64e485e5a77c8777cc31ee185213d26df4eb6a0a
AleksBG44 <800077413@fvtc.edu>
2026-04-19 16:55:54 -0500

    create backend routes

--- routes/api/v1/events.js
+++ routes/api/v1/events.js
@@ -1,6 +1,34 @@
 const router = require('express').Router()
 const { getCollection, ObjectId } = require('../../../dbconnect')
 
-// todo:API
+let collection = null
+const getEvents = async () => {
+    if (!collection) collection = await getCollection('FoodTruckAPI', 'events')
+        return collection
+}   
+
+router.get('/:id', async (request, response) => {
+    const { id } = request.params
+    const collection = await getEvents()
+    const found = await collection.findOne({ _id: new ObjectId(id) })
+    if (found) response.send(found)
+    else response.send({ error: { message: `Could not find event with id: ${id}` }})
+})
+
+router.get('/', async (request, response) => {
+    const collection = await getEvents()
+    const found = await collection.find().toArray()
+    if (found) response.send(found)
+    else response.send({error: { message: `Could not find events.` }})
+})
+
+router.post("/", async (request, response) => {
+    const { name, location, date, time } = request.body
+    const collection = await getEvents()
+    const result = await collection.insertOne({ name, location, date, time })
+    const { acknowledged, insertedId } = result
+    response.send({ acknowledged, insertedId })
+})
+
 
 module.exports = router
--- routes/api/v1/menu.js
+++ routes/api/v1/menu.js
@@ -1,6 +1,34 @@
 const router = require('express').Router()
+const { request, response } = require('express')
 const { getCollection, ObjectId } = require('../../../dbconnect')
 
+let collection = null
+const getMenu = async () => {
+    if (!collection) collection = await getCollection('FoodTruckAPI', 'menu')
+        return collection
+}   
 
+router.get('/:id', async (request, response) => {
+    const { id } = request.params
+    const collection = await getMenu()
+    const found = await collection.findOne({ _id: new ObjectId(id) })
+    if (found) response.send(found)
+    else response.send({ error: { message: `Could not find menu item with id: ${id}` }})
+})
+
+router.get('/', async (request, response) => {
+    const collection = await getMenu()
+    const found = await collection.find().toArray()
+    if (found) response.send(found)
+    else response.send({error: { message: `Could not find menu.` }})
+})
+
+router.post("/", async (request, response) => {
+    const { name, description, price, url } = request.body
+    const collection = await getMenu()
+    const result = await collection.insertOne({ name, description, price, url })
+    const { acknowledged, insertedId } = result
+    response.send({ acknowledged, insertedId })
+})
 
 module.exports = router
