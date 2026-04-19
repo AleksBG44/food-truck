445c4e475edcee794f8ac2b785d9626ab460d0a2
Aleks <vavilonovboris@gmail.com>
2026-04-19 13:18:19 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.use('/api/v1/menu', require('./routes/api/v1/menu'))
+app.use('/api/v1/events', require('./routes/api/v1/events'))
+app.use(require('./routes/static'))
+
+app.listen(port, () => console.log(`http://localhost:${port}`))
---
+++ routes/api/v1/events.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const { getCollection, ObjectId } = require('../../../dbconnect')
+
+
+
+module.exports = router
